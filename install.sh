ab7798e166e54a829441487bdede1fbc9c661f7a
acxz <17132214+acxz@users.noreply.github.com>
2024-10-20 21:49:48 -0400

    update install dir variable

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-system_user_dir="/usr/local/"
+install_dir="/usr/local/"
 
-mkdir -pv "${1:-${system_user_dir}}"
-cp -rv bin "${1:-${system_user_dir}}"
+mkdir -pv "${1:-${install_dir}}"
+cp -rv bin "${1:-${install_dir}}"
--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-system_user_dir="/usr/local/"
+install_dir="/usr/local/"
 
-rm -v "${1:-${system_user_dir}}bin/wallshell"
-rm -rv "${1:-${system_user_dir}}bin/imageshell"
-rmdir -v "${1:-${system_user_dir}}bin"
+rm -v "${1:-${install_dir}}bin/wallshell"
+rm -rv "${1:-${install_dir}}bin/imageshell"
+rmdir -v "${1:-${install_dir}}bin"
